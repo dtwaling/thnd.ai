d8a54e451ce9d6edb20f1460d6f9dc1ca9e940a6
duswali <dustin@walings.com>
2025-10-19 20:24:40 -0400

    Simplify-n-refine; phase 3 - Dependent file updates
     - Update remaining files depending upon the changed paths. (plus a few refinements)
     - Remove old folders and files no longer needed, or moved to new locations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,47 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import sitemap from '@astrojs/sitemap';
-//import starlightObsidian, { obsidianSidebarGroup } from 'starlight-obsidian';
+import node from '@astrojs/node';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://thnd.ai',
-	integrations: [
-		starlight({
-			title: 'THND.ai',
-			logo: {
-				src: './public/images/bolt-icon.png',
-			},
-			favicon: '/favicon.png',
-			social: [
-				{ icon: 'x.com', label: 'X.com', href: 'https://x.com/thndai' },
-				{ icon: 'telegram', label: 'Telegram', href: 'https://t.me/orderofhodl' },
-			],
-			customCss: [
-				"./src/styles/custom.css",
-			],
-		}),
-		sitemap()
-	],
-	server: {
-	  host: true,
-	  port: 4321,
-	},
+  site: 'https://thnd.ai',
+  base: '/',
+  publicDir: './public',
+
+  integrations: [
+    starlight({
+      title: 'THND.ai',
+      logo: {
+        src: './public/favicon/favicon.png',
+      },
+      favicon: '/favicon/favicon.svg',
+      social: [
+        { icon: 'x.com', label: 'X.com', href: 'https://x.com/thndai' },
+        { icon: 'telegram', label: 'Telegram', href: 'https://t.me/orderofhodl' },
+      ],
+      customCss: [
+        "./src/styles/custom.css",
+      ],
+    }),
+    mdx(),
+    sitemap()
+  ],
+
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
+
+  // server: ({ command }) => ({
+  //   port: command === 'dev' ? 4321 : 8080,
+  //   host: true,
+  // }),
+
+  // outDir: './dist',
+  // output: 'server',
+  // build: {
+  //   client: './client',
+  //   server: './server',
+  // },
 });
