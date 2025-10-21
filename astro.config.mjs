c581e72c3ee7a90d849475b37ef696694983fbeb
duswali <dustin@walings.com>
2025-10-21 07:21:09 -0400

    Attempt to fix image render issue in prod..

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   site: 'https://thnd.ai',
   publicDir: './public',
   adapter: cloudflare({
-    imageService: 'cloudflare'
+    imageService: 'compile'
   }),
   vite: {
     build: {
